21d8dbe72df2cf1d551a314920d77b8e24ac430d
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-10-22 18:06:14 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,1 +1,18 @@
+"""4 пробела!!!"""
+import requests
+from bs4 import BeautifulSoup
+class CinemaParser:
+    ''' Этот класс очень нужен'''
+    def __init__(self, city='msk'):
+        ''' Узнаём город'''
+        self.city = city
+        self.content = ''
+    def extract_raw_content(self):
+        ''' Скачиваем HTML с сайта '''
+        url = 'https://msk.subscity.ru'
+        self.content = requests.get(url)
+    def print_raw_content(self):
+        '''Красивый вывод HTML'''
+        soup = BeautifulSoup(self.content.text, 'html.parser')
+        return soup.prettify()
 
