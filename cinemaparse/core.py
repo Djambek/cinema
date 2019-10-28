36a26d1038fe5a88db0d63fd7d3061d9946aeec3
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-10-28 10:25:25 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -7,6 +7,7 @@ class CinemaParser:
         ''' Узнаём город'''
         self.city = city
         self.content = ''
+        self.film = []
     def extract_raw_content(self):
         ''' Скачиваем HTML с сайта '''
         url = 'https://msk.subscity.ru'
@@ -15,4 +16,12 @@ class CinemaParser:
         '''Красивый вывод HTML'''
         soup = BeautifulSoup(self.content.text, 'html.parser')
         return soup.prettify()
-
+    def found(self):
+        ''' Это фунция ведёт поиск тегов с фильмами'''
+        soup = BeautifulSoup(self.content.text, 'html.parser')
+        #print(soup.find_all('div', class_="movie-titles"))
+        for i in soup.find_all('div', class_="movie-titles"):
+            for j in i.find_all('div', class_="movie-title"):
+                for key in j.find_all('a', class_="underdashed"):
+                    self.film.append(key.text.replace('\xad', ''))
+        return self.film
