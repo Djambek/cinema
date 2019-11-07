5950a7cb6692f3e8342b102d568417c846c75fc8
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-11-07 18:41:35 +0300

    Update test.py

--- cinemaparse/test.py
+++ cinemaparse/test.py
@@ -2,5 +2,10 @@
 from core import CinemaParser
 PR = CinemaParser()
 PR.extract_raw_content()
-print(PR.print_raw_content())
+print("Введите название фильма, который Вы хотите посмотреть")
+i = input()
+print("Введите название кинотеатра")
+b = input()
 print(PR.found())
+print(PR.sesion(i))
+print(PR.get_nearest_subway_station(b))
