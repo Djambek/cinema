63aeca446ee8aca284a52cf9f839e5e096c495ca
Dzambek <44769905+Dzambek@users.noreply.github.com>
2019-11-12 20:00:06 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
 '''Этот модуль предназначен для тестирования класса CinemaParser '''
-from core import CinemaParser
-pr = CinemaParser()
-pr.extract_raw_content()
-print(pr.print_raw_content())
-
+import cinema
+PR = cinema()
+PR.extract_raw_content()
+print("Введите название фильма")
+i = input()
+print("Введите название кинотеатра")
+b = input()
+print(PR.get_films_list())
+PR.some_cycles(i)
+print(PR.get_film_nearest_session())
+print(PR.get_nearest_subway_station(b))
